88fe037bc8f26593ded05cc14214aa7c2196e9ac
Ashutosh Mahajan <abm523@nyu.edu>
2019-04-23 22:24:07 -0400

    Created Post and Message models

---
+++ Data/Models/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    class Message
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int messageID { get; set; }
+
+        [Required]
+        public DateTime messageTimestamp { get; set; }
+
+        [Required]
+        public String senderID { get; set; }
+
+        [Required]
+        public String receiverID { get; set; }
+
+
+
+    }
+}
---
+++ Data/Models/Post.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    class Post
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public int postNumber { get; set; }
+
+        [Required]
+        public DateTime postTimestamp { get; set; }
+
+        [Required]
+        public DateTime postExpiration { get; set; }
+
+        [Required]
+        public String ownerID { get; set; }
+
+        [Required]
+        public String title { get; set; }
+
+        [DefaultValue("")]
+        public String body { get; set; }
+
+        [Required]
+        public String area { get; set; }
+
+        [DefaultValue("")]
+        public String locale { get; set; }
+
+        [Required]
+        public String category { get; set; }
+
+        [DefaultValue("")]
+        public String subcategory { get; set; }
+
+        [DefaultValue(false)]
+        public bool isDeletedOrHidden { get; set; }
+
+        [DefaultValue(true)]
+        public bool canBeModified { get; set; }
+
+        public ICollection<Message> messages { get; set; }
+
+    }
+}
